55b9b4cd6306de76482ea7d897e0b1982d2712e5
editorgis <editorgis@gmail.com>
2026-06-05 16:16:32 -0500

    excel_dj_2025.py

--- excel_dj_2025.py
+++ excel_dj_2025.py
@@ -64,7 +64,7 @@ archivo_excel = st.session_state['base_datos']
 nombres_hojas = st.session_state['hojas']
 
 # --- 7. INTERFAZ VISUAL ---
-st.markdown("<h1 style='text-align: center; color: #1E3A8A;'>🏛️ SISTEMA DE CONSULTA RÁPIDA DECLARACIÓN JURADA 2025 - ICA</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: #1E3A8A;'>🏛️ SISTEMA DE CONSULTA RÁPIDA DECLARACIÓN JURADA 2025 - ICA demo.v2</h1>", unsafe_allow_html=True)
 st.markdown("<p style='text-align: center; color: #1E3A8A; font-weight: bold;'>🚫 AVISO: Este sistema contiene información reservada. Está prohibido el acceso no autorizado bajo denuncia de la Ley No. 29733 Protección de Datos</p>", unsafe_allow_html=True)
 
 col_status, _, col_logout = st.columns([3, 4, 1])
